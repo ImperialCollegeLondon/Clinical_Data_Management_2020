c5dc113fb410e9cd3612583c1f409b4828e82510
Wenjia Bai <w.bai@imperial.ac.uk>
2020-08-30 13:05:33 +0100

    Updated

---
+++ practical_04/practical_04.sql
@@ -0,0 +1,16 @@
+-- Use the sample database
+USE classicmodels;
+
+-- Show indexes used by table employees
+-- We provide the first example, you can click Query | Execute Current Statement to see the results.
+SHOW INDEX
+FROM employees;
+
+-- The following are exercises that you need to complete or answer.
+-- Show indexes used by table orders
+
+
+-- Show indexes used by table customers
+
+
+-- What data structure do these indexex use?
